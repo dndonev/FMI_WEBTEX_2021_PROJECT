58a9bf5cca6be718b6c8195aa4036c488e7c7076
Dobrin Donev <dobrin.donev@taryafintech.com>
2021-03-19 17:01:27 +0200

    Remove http and use express only

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-const http = require('http').Server(app)
-
 const appConfig = require('./app-config.json')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,7 +16,7 @@ app.get('*', function (req, res) {
     res.status(404).send()
 })
 
-http.listen(appConfig.server.port, () => {
+app.listen(appConfig.server.port, () => {
     mongoose.connect(appConfig.mongodb.connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`Server listening on port ${appConfig.server.port}`))
         .catch(err => console.log(err))
