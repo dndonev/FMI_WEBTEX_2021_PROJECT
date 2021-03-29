e86118fbfac2f2065fd465c8d4d2815f427d6181
Dobrin Donev <d.d.donev@abv.bg>
2021-03-29 23:52:54 +0300

    Add initial main screen styles

--- client/src/components/main/main.component.tsx
+++ client/src/components/main/main.component.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 import './main.styles.scss'
 
+import SideBarComponent from './../sidebar/sidebar.component'
+
 const MainComponent = () => {
 	return (
 		<div className="main-container">
 			<div className="header">
 				<button className="login-button" type="button">log out</button>
 			</div>
-			<div className="siderbar"></div>
+			<div className="sidebar">
+				<SideBarComponent />
+			</div>
 			<div className="files-container">
 				<div className="search-box">to be component</div>
 				<div className="file-container"></div>
