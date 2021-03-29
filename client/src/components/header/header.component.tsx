5c1d58639a649c094b84b903986db682f9d3a930
Dobrin Donev <d.d.donev@abv.bg>
2021-03-30 01:30:20 +0300

    Extract header component

---
+++ client/src/components/header/header.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './header.styles.scss'
+
+const HeaderComponent = () => {
+	return (
+		<div className="header">
+			<button className="login-button" type="button">log out</button>
+		</div>
+	)
+}
+
+export default HeaderComponent;
--- client/src/components/main/main.component.tsx
+++ client/src/components/main/main.component.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 import './main.styles.scss'
 
 import SideBarComponent from './../sidebar/sidebar.component'
+import HeaderComponent from './../header/header.component'
 
 const MainComponent = () => {
 	return (
 		<div className="main-container">
-			<div className="header">
-				<button className="login-button" type="button">log out</button>
-			</div>
+			<HeaderComponent />
 			<div className="middle-container">
 				<div className="sidebar">
 					<SideBarComponent />
