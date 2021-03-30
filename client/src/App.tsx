8ec636fbf6bc267b85865afae7c554de952c4531
alexandermalinov <alexandermalinov99@gmail.com>
2021-03-30 21:42:03 +0300

    Adding login modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.scss';
 
 import HomeComponent from './components/home/home.component'
+import LoginComponent from './components/login/login.component'
 
 const  App = () => {
   return (
     <div className="App">
-      <HomeComponent/>
+      <LoginComponent />
     </div>
   );
 }
