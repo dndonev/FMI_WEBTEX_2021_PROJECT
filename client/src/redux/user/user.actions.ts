d9ca307eb0d0fc5a18c77228eb01e04c7beacaf2
kstankova <kstankova@astea.net>
2021-05-02 01:34:02 +0300

    created redux actions for user login/register

---
+++ client/src/redux/user/user.actions.ts
@@ -0,0 +1,51 @@
+import { LoginState, RegisterState, User, UserActionTypes } from "./user.types";
+
+export interface IUserBaseAction {
+    type: UserActionTypes;
+}
+
+export interface IRegisterUser extends IUserBaseAction {
+    type: UserActionTypes.RegisterUser;
+    data: RegisterState;
+}
+
+export interface IRegisterSuccess extends IUserBaseAction {
+    type: UserActionTypes.RegisterSuccess;
+}
+
+export interface IRegisterError extends IUserBaseAction {
+    type: UserActionTypes.RegisterError;
+    data: string;
+}
+
+export interface ILoginUser extends IUserBaseAction {
+    type: UserActionTypes.LoginUser;
+    data: LoginState;
+}
+
+export interface ILoginSuccess extends IUserBaseAction {
+    type: UserActionTypes.LoginSuccess;
+    data: User;
+}
+
+export interface ILoginError extends IUserBaseAction {
+    type: UserActionTypes.LoginError;
+    data: string;
+}
+
+export interface ILogoutUser extends IUserBaseAction {
+    type: UserActionTypes.LogoutUser;
+}
+
+export interface ILogoutSucces extends IUserBaseAction {
+    type: UserActionTypes.LogoutSuccess;
+}
+
+export interface ILogoutError extends IUserBaseAction {
+    type: UserActionTypes.LogoutError;
+    data: string;
+}
+
+export type TUserReducerActions = IRegisterUser | IRegisterSuccess | IRegisterError
+                                | ILoginUser | ILoginSuccess | ILoginError 
+                                | ILogoutUser | ILogoutSucces | ILogoutError;
---
+++ client/src/redux/user/user.types.ts
@@ -0,0 +1,37 @@
+export enum UserActionTypes {
+    LoginUser = 'LOGIN_USER',
+    LoginSuccess = 'LOGIN_SUCCESS',
+    LoginError = 'LOGIN_ERROR',
+    RegisterUser = 'REGISTER_USER',
+    RegisterSuccess = 'REGISTER_SUCCESS',
+    RegisterError = 'REGISTER_ERROR',
+    LogoutUser = 'LOGOUT_USER',
+    LogoutSuccess = 'LOGOUT_SUCCESS',
+    LogoutError = 'LOGOUT_ERROR'
+}
+
+export interface RegisterState {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface LoginState {
+    email: string;
+    password: string;
+}
+
+export interface UserState {
+    awaitingAuthentication: boolean;
+    authenticated: boolean;
+    registeredUser: boolean;
+    currentUser: User;
+}
+
+export interface User {
+    email: string;
+    username: string;
+    firstName?: string;
+    lastName?: string;
+}
